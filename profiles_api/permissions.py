ab986383a684db506d56a77529d3013a2b26a78b
Prakash KG <prakashgowinji@gmail.com>
2021-06-05 19:47:41 +0530

    Added user profile feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,3 +10,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow Users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the uesr is tring to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
